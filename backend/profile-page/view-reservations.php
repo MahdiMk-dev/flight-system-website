f5c2712e3d8ee200dcbda39db710ccdb9e8c2234
kamilawad1 <kamilawad15@gmail.com>
2024-03-16 19:26:54 +0200

    added cancel reservation api

--- backend/profile-page/view-reservations.php
+++ backend/profile-page/view-reservations.php
@@ -5,7 +5,7 @@ $user_id = $_GET['user_id'];
 
 $statusUp = 'upcoming';
 
-$query = $mysqli->prepare('select flight_id, departure_date from flights join reservations on flights.id = reservations.flight_id where user_id = ? and status = ?');
+$query = $mysqli->prepare('select reservations.id, departure_date from flights join reservations on flights.id = reservations.flight_id where user_id = ? and status = ?');
 $query->bind_param('is', $user_id, $statusUp);
 $query->execute();
 $query->store_result();
@@ -14,11 +14,11 @@ $num_rows = $query->num_rows();
 if ($num_rows == 0) {
     $response['status'] = 'no bookings';
 } else {
-    $query->bind_result($flight_id, $date);
+    $query->bind_result($booking_id, $date);
     $bookings = [];
     while ($query->fetch()) {
         $booking =[
-            'flight_id' => $flight_id,
+            'booking_id' => $booking_id,
             'date' => $date
         ];
     $bookings[] = $booking;
