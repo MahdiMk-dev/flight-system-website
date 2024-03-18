d10cf0982da1aa4025ebc7c2d7e20ea2e7e34d08
Mahdi Mokalled <mokaledmahdi@gmail.com>
2024-03-18 23:49:01 +0200

    update db schema

--- backend/db/flight-system-website (2).sql
+++ backend/db/flight-system-website (2).sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3306
--- Generation Time: Mar 17, 2024 at 10:56 PM
--- Server version: 8.2.0
--- PHP Version: 8.2.13
+-- Host: localhost
+-- Generation Time: Mar 18, 2024 at 08:17 PM
+-- Server version: 8.0.31
+-- PHP Version: 7.4.33
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -27,29 +27,22 @@ SET time_zone = "+00:00";
 -- Table structure for table `airlines`
 --
 
-DROP TABLE IF EXISTS `airlines`;
-CREATE TABLE IF NOT EXISTS `airlines` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  `origin` varchar(255) NOT NULL,
-  `rate` int NOT NULL,
-  `review` text NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- --------------------------------------------------------
+CREATE TABLE `airlines` (
+  `id` int NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `country` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `headquarters` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `website` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Table structure for table `airplane-models`
+-- Dumping data for table `airlines`
 --
 
-DROP TABLE IF EXISTS `airplane-models`;
-CREATE TABLE IF NOT EXISTS `airplane-models` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  `capacity` int NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+INSERT INTO `airlines` (`id`, `name`, `country`, `headquarters`, `website`) VALUES
+(1, 'Middle East Airlines', 'Lebanon', 'Beirut, Lebanon', 'https://www.mea.com.lb'),
+(2, 'Turkish Airlines', 'Turkey', 'Istanbul, Turkey', 'https://www.turkishairlines.com'),
+(3, 'Qatar Airways', 'Qatar', 'Doha, Qatar', 'https://www.qatarairways.com');
 
 -- --------------------------------------------------------
 
@@ -57,13 +50,45 @@ CREATE TABLE IF NOT EXISTS `airplane-models` (
 -- Table structure for table `airplanes`
 --
 
-DROP TABLE IF EXISTS `airplanes`;
-CREATE TABLE IF NOT EXISTS `airplanes` (
-  `id` int NOT NULL AUTO_INCREMENT,
+CREATE TABLE `airplanes` (
+  `id` int NOT NULL,
   `model_id` int NOT NULL,
-  `airline_id` int NOT NULL,
-  PRIMARY KEY (`id`)
+  `airline_id` int NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `airplanes`
+--
+
+INSERT INTO `airplanes` (`id`, `model_id`, `airline_id`) VALUES
+(1, 1, 1),
+(2, 1, 1),
+(3, 2, 1),
+(4, 3, 2),
+(5, 2, 2),
+(6, 3, 1),
+(7, 2, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `airplane_models`
+--
+
+CREATE TABLE `airplane_models` (
+  `id` int NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `capacity` int NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `airplane_models`
+--
+
+INSERT INTO `airplane_models` (`id`, `name`, `capacity`) VALUES
+(1, 'Airbus', 100),
+(2, 'Bombardier', 50),
+(3, 'Embraer', 50);
 
 -- --------------------------------------------------------
 
@@ -71,13 +96,22 @@ CREATE TABLE IF NOT EXISTS `airplanes` (
 -- Table structure for table `airports`
 --
 
-DROP TABLE IF EXISTS `airports`;
-CREATE TABLE IF NOT EXISTS `airports` (
+CREATE TABLE `airports` (
   `id` int NOT NULL,
-  `origin` varchar(255) NOT NULL,
-  `capacity` int NOT NULL,
-  `name` varchar(255) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `name` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `city` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `country` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `code` varchar(3) COLLATE utf8mb4_general_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `airports`
+--
+
+INSERT INTO `airports` (`id`, `name`, `city`, `country`, `code`) VALUES
+(1, 'Beirut-Rafic Hariri International Airport', 'Beirut', 'Lebanon', 'BEY'),
+(2, 'Hamad International Airport', 'Doha', 'Qatar', 'DOH'),
+(3, 'Istanbul Airport', 'Istanbul', 'Turkey', 'IST');
 
 -- --------------------------------------------------------
 
@@ -85,15 +119,13 @@ CREATE TABLE IF NOT EXISTS `airports` (
 -- Table structure for table `emergency-contacts`
 --
 
-DROP TABLE IF EXISTS `emergency-contacts`;
-CREATE TABLE IF NOT EXISTS `emergency-contacts` (
-  `id` int NOT NULL AUTO_INCREMENT,
+CREATE TABLE `emergency-contacts` (
+  `id` int NOT NULL,
   `user_id` int NOT NULL,
   `name` varchar(255) NOT NULL,
   `phone_number` int NOT NULL,
   `email` varchar(255) NOT NULL,
-  `relation` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
+  `relation` varchar(255) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
@@ -102,9 +134,8 @@ CREATE TABLE IF NOT EXISTS `emergency-contacts` (
 -- Table structure for table `flights`
 --
 
-DROP TABLE IF EXISTS `flights`;
-CREATE TABLE IF NOT EXISTS `flights` (
-  `id` int NOT NULL AUTO_INCREMENT,
+CREATE TABLE `flights` (
+  `id` int NOT NULL,
   `price` int NOT NULL,
   `departure_airport_id` int NOT NULL,
   `arrival_airport_id` int NOT NULL,
@@ -112,9 +143,8 @@ CREATE TABLE IF NOT EXISTS `flights` (
   `departure_time` time NOT NULL,
   `arrival_date` date NOT NULL,
   `arrival_time` time NOT NULL,
-  `airplane_id` int NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `airplane_id` int NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `flights`
@@ -129,13 +159,11 @@ INSERT INTO `flights` (`id`, `price`, `departure_airport_id`, `arrival_airport_i
 -- Table structure for table `reservations`
 --
 
-DROP TABLE IF EXISTS `reservations`;
-CREATE TABLE IF NOT EXISTS `reservations` (
-  `id` int NOT NULL AUTO_INCREMENT,
+CREATE TABLE `reservations` (
+  `id` int NOT NULL,
   `flight_id` int NOT NULL,
   `user_id` int NOT NULL,
-  `seat_number` int NOT NULL,
-  PRIMARY KEY (`id`)
+  `seat_number` int NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
@@ -144,28 +172,121 @@ CREATE TABLE IF NOT EXISTS `reservations` (
 -- Table structure for table `users`
 --
 
-DROP TABLE IF EXISTS `users`;
-CREATE TABLE IF NOT EXISTS `users` (
-  `id` int NOT NULL AUTO_INCREMENT,
+CREATE TABLE `users` (
+  `id` int NOT NULL,
   `username` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `dob` date NOT NULL,
-  `nationality` varchar(255) NOT NULL,
-  `passport_number` int NOT NULL,
-  `phone_number` int NOT NULL,
-  `coins` int NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `dob` date DEFAULT NULL,
+  `nationality` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `passport_number` int DEFAULT NULL,
+  `phone_number` int DEFAULT NULL,
+  `coins` int DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
--- Dumping data for table `users`
+-- Indexes for dumped tables
 --
 
-INSERT INTO `users` (`id`, `username`, `email`, `password`, `dob`, `nationality`, `passport_number`, `phone_number`, `coins`) VALUES
-(1, 'test12', 'test@gmail.com', '$2y$10$TOsMor7coAAKVbgKiEwgJ.42HQVfEhsKkrkU8XPx.Yfg66d9lfs0W', '0000-00-00', '', 0, 0, 0),
-(2, 'mahdi', 'mokaledmahdi@gmail.com', '$2y$10$G565Tz/gvek0cfkcaSB8Y.KTGeB3TzbWVUbRulmLSCCXYsNHzlCge', '0000-00-00', '', 0, 0, 0),
-(3, 'mahdi', 'mahdi@gmail.com', '$2y$10$AxqucklYuyTrrmRWRS1DV.Bo2o8Srvy5rzC9XGMcnreWV8ExbbGeG', '0000-00-00', '', 0, 0, 0);
+--
+-- Indexes for table `airlines`
+--
+ALTER TABLE `airlines`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `airplanes`
+--
+ALTER TABLE `airplanes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `airplane_models`
+--
+ALTER TABLE `airplane_models`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `airports`
+--
+ALTER TABLE `airports`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `emergency-contacts`
+--
+ALTER TABLE `emergency-contacts`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `flights`
+--
+ALTER TABLE `flights`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `reservations`
+--
+ALTER TABLE `reservations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `airlines`
+--
+ALTER TABLE `airlines`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `airplanes`
+--
+ALTER TABLE `airplanes`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `airplane_models`
+--
+ALTER TABLE `airplane_models`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `airports`
+--
+ALTER TABLE `airports`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `emergency-contacts`
+--
+ALTER TABLE `emergency-contacts`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `flights`
+--
+ALTER TABLE `flights`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `reservations`
+--
+ALTER TABLE `reservations`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
